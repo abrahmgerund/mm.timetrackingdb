704e68d061f438455b91231826ea5f2217594111
abrahmgerund <abrahmgerund@gmail.com>
2025-08-14 22:26:42 -0600

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,79 +2,64 @@ const dailyBtn = document.querySelector('#daily');
 const weeklyBtn = document.querySelector('#weekly');
 const monthlyBtn = document.querySelector('#monthly');
 
-// fetch('../data.json')
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch((error) => console.log('error warning'));
+
+let userData = [];
+
+const fetchUser = function () {
+    fetch('./data.json')
+        .then((response) => response.json())
+        .then((data) => userData = data)
+        .catch((error) => console.log('error warning'));
+}
 
 const getDaily = function () {
-    fetch('./data.json')
-    .then((response) => response.json())
-    .then((data) => {
         const h1s = document.querySelectorAll('h1');
         h1s.forEach((h1, index) => {
-                h1.innerText = `${data[index].timeframes.daily.current}hrs`;
+                h1.innerText = `${userData[index].timeframes.daily.current}hrs`;
         });
 
         const ps = document.querySelectorAll('h1 + p');
         ps.forEach((p, index) => {
-                p.innerText = `Yesterday - ${data[index].timeframes.daily.previous}hrs`;
+                p.innerText = `Yesterday - ${userData[index].timeframes.daily.previous}hrs`;
         });
-
         // console.log(data[0].timeframes.daily.current);
-
-    })
-    .catch((error) => console.log('error warning'));
-}
+};
 
 const getWeekly = function () {
-    fetch('./data.json')
-    .then((response) => response.json())
-    .then((data) => {
         const h1s = document.querySelectorAll('h1');
         h1s.forEach((h1, index) => {
-                h1.innerText = `${data[index].timeframes.weekly.current}hrs`;
+                h1.innerText = `${userData[index].timeframes.weekly.current}hrs`;
         });
 
         const ps = document.querySelectorAll('h1 + p');
         ps.forEach((p, index) => {
-                p.innerText = `Last Week - ${data[index].timeframes.weekly.previous}hrs`;
+                p.innerText = `Last Week - ${userData[index].timeframes.weekly.previous}hrs`;
         });
-
         // console.log(data[0].timeframes.daily.current);
-
-    })
-    .catch((error) => console.log('error warning'));
-}
+};
 
 const getMonthly = function () {
-    fetch('./data.json')
-    .then((response) => response.json())
-    .then((data) => {
         const h1s = document.querySelectorAll('h1');
         h1s.forEach((h1, index) => {
-                h1.innerText = `${data[index].timeframes.monthly.current}hrs`;
+                h1.innerText = `${userData[index].timeframes.monthly.current}hrs`;
         });
 
         const ps = document.querySelectorAll('h1 + p');
         ps.forEach((p, index) => {
-                p.innerText = `Last Month - ${data[index].timeframes.monthly.previous}hrs`;
+                p.innerText = `Last Month - ${userData[index].timeframes.monthly.previous}hrs`;
         });
-
         // console.log(data[0].timeframes.daily.current);
-
-    })
-    .catch((error) => console.log('error warning'));
-}
+};
 
 const weeklyClicked = function () {
     document.querySelector('#weekly').checked = true;
 }
 
 
+
+fetchUser();
+
 dailyBtn.addEventListener('click', getDaily);
 weeklyBtn.addEventListener('click', getWeekly);
 monthlyBtn.addEventListener('click', getMonthly);
 document.addEventListener('DOMContentLoaded', weeklyClicked);
-
-// console.log(document.querySelectorAll('h1'));
